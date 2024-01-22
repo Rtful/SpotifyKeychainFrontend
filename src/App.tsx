6fe7ba52f1affbb8bd7c2e1ade6efbf950d4b788
Aaron Holenstein <4aron.dodger@gmail.com>
2024-01-22 22:28:58 +0100

    started implementing the search functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,67 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const CLIENT_ID = "35e420fcea2b456ba34b98c24b1610b9"
+const CLIENT_SECRET = "e80fddb0c3f247e28d1b1afe887049a3"
+
+
 function App() {
-    const [count, setCount] = useState(0)
+    const [searchString, setSearchString] = useState<string>('')
     const [url, setUrl] = useState<string>('')
+    const [filename, setFilename] = useState<string>('spotifyCode')
+    const [accessToken, setAccessToken] = useState<string>('')
+
+    // API access token
+    useEffect(() => {
+        const authParameters = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
+        }
+        fetch('https://accounts.spotify.com/api/token', authParameters)
+            .then(result => result.json())
+            .then(data => setAccessToken(data))
+    }, [])
+
+    // Search
+    async function search() {
+        let artistParameters = {
+            method:'GET',
+            headers:{
+                'Content-Type':'application/json',
+                'Authorization':accessToken
+            }
+        }
+        const artistID = await fetch('https://api.spotify.com/v1/search?q=' + searchString + '&type=artist', artistParameters)
+            .then(response => response.json())
+            .then(data => {return data.artists.items[0].id})
+        console.log('searching for "' + searchString + '"')
+        console.log('Artist ID "' + artistID + '"')
+    }
+
+    // download
+    function download3dModel() {
+        const data = new URLSearchParams();
+        data.append('url', url);
+        fetch('http://spotifycode.dodger.ch:5000/getCode', {
+            method: 'POST',
+            mode: 'cors',
+            body: data
+        }).then(response => response.blob())
+            .then(blob => {
+                const fileUrl = window.URL.createObjectURL(blob)
+                const link = document.createElement('a')
+                link.href = fileUrl
+                link.setAttribute('download', filename + ".stl") // Set the desired file name and extension
+                document.body.appendChild(link)
+                link.click()
+                link.remove()
+            })
+    }
 
     return (
         <>
@@ -17,14 +73,16 @@ function App() {
                     <img src={reactLogo} className="logo react" alt="React logo"/>
                 </a>
             </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
+            <div>
+                <input
+                    value={searchString}
+                    onChange={e => {
+                        setSearchString(e.target.value);
+                    }}
+                />
+                <button onClick={() => {
+                    search();
+                }}></button>
             </div>
             <div>
                 <input
@@ -34,7 +92,7 @@ function App() {
                     }}
                 />
                 <button onClick={() => {
-                    download3dModel(url, "spotifyCode");
+                    download3dModel();
                 }}></button>
             </div>
             <p className="read-the-docs">
@@ -42,25 +100,6 @@ function App() {
             </p>
         </>
     )
-}
-
-function download3dModel(url: string, filename:string) {
-    const data = new URLSearchParams();
-    data.append('url', url);
-    fetch('http://spotifycode.dodger.ch:5000/getCode', {
-        method: 'POST',
-        mode: 'cors',
-        body: data
-    }).then(response => response.blob())
-        .then(blob => {
-            const fileUrl = window.URL.createObjectURL(blob);
-            const link = document.createElement('a');
-            link.href = fileUrl;
-            link.setAttribute('download', filename); // Set the desired file name and extension
-            document.body.appendChild(link);
-            link.click();
-            link.remove();
-        });
 }
 
 export default App
